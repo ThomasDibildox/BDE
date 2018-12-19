84dad5ddf45e93c8df3ddff796c3b94b19141cc9
Thomas Dibildox <dibildox.thomas@gmail.com>
2018-12-19 17:50:06 +0100

    Init2

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+include "getRacine.php";
+include "$racine/controleur/controleurPrincipal.php";
+
+if (isset($_GET["action"])){
+    $action = $_GET["action"];
+}
+else{
+    
+    $action = "defaut";
+}
+
+$fichier = controleurPrincipal($action);
+include "$racine/controleur/$fichier";
+
+
+?>
+     
